da6ec39f7f335e8126641f156115626b9548b441
jechs83 <jechs83@gmail.com>
2023-02-25 04:18:06 -0500

    update

--- plazavea/plazavea/spiders/url_list.py
+++ plazavea/plazavea/spiders/url_list.py
@@ -1,8 +1,14 @@
-list1 = [  ("https://www.plazavea.com.pe/api/catalog_system/pub/products/search?fq=C:/678/&_from=", "&_to=", "&O=OrderByScoreDESC&"),
+list1 = [  ("https://www.plazavea.com.pe/api/catalog_system/pub/products/search?fq=C:/678/&_from=", "&_to=", "&O=OrderByScoreDESC&"),#tecnologia
     
-("https://www.plazavea.com.pe/api/catalog_system/pub/products/search?fq=C:/679/&_from=", "&_to=", "&O=OrderByScoreDESC&"),
-("https://www.plazavea.com.pe/api/catalog_system/pub/products/search?fq=C:/1098/&_from=", "&_to=","&O=OrderByScoreDESC&"),
-
+("https://www.plazavea.com.pe/api/catalog_system/pub/products/search?fq=C:/679/&_from=", "&_to=", "&O=OrderByScoreDESC&"),#electro
+("https://www.plazavea.com.pe/api/catalog_system/pub/products/search?fq=C:/1098/&_from=", "&_to=","&O=OrderByScoreDESC&"),#muebles
+("https://www.plazavea.com.pe/api/catalog_system/pub/products/search?fq=C:/1409/&_from=", "&_to=","&O=OrderByScoreDESC&"),#dormito
+("https://www.plazavea.com.pe/api/catalog_system/pub/products/search?fq=C:/681/&_from=", "&_to=","&O=OrderByScoreDESC&"),#deco hogra
+("https://www.plazavea.com.pe/api/catalog_system/pub/products/search?fq=C:/1105/&_from=", "&_to=","&O=OrderByScoreDESC&"),#deportes
+("https://www.plazavea.com.pe/api/catalog_system/pub/products/search?fq=C:/1258/&_from=", "&_to=","&O=OrderByScoreDESC&"),# moda
+("https://www.plazavea.com.pe/api/catalog_system/pub/products/search?fq=C:/1761/&_from=", "&_to=","&O=OrderByScoreDESC&"),# calzado
+("https://www.plazavea.com.pe/api/catalog_system/pub/products/search?fq=C:/922/&_from=", "&_to=","&O=OrderByScoreDESC&"),# juguetes
+("https://www.plazavea.com.pe/api/catalog_system/pub/products/search?fq=C:/898/&_from=", "&_to=","&O=OrderByScoreDESC&"),# mejoramiento hogas
 ]
 
 
