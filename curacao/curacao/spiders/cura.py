8e7f717b1f89183b3e05a79db9313ce924e5244a
jechs83 <jechs83@gmail.com>
2023-02-25 19:55:59 -0500

    udpdaye

--- curacao/curacao/spiders/cura.py
+++ curacao/curacao/spiders/cura.py
@@ -36,13 +36,13 @@ class CuraSpider(scrapy.Spider):
                 urls = url_list.list4
         else:
             urls = []
-
+        count = 0
         for i, v in enumerate(urls):
             for e in range(v[2]):
                 if e == 0:
                      url = v[0]+str(0)+v[1]
                 else:   
-                    url = v[0]+str(18*(18*(e+1)))+v[1]
+                    url = v[0]+str(count*e)+v[1]
                 yield scrapy.Request(url, self.parse)
 
 
