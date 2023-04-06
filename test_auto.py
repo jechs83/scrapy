7c6c29f5f2e2047459a216d38cdf44b6543fcabd
jechs <jechs83@gmail.com>
2023-04-06 00:34:30 -0500

    update

--- test_auto.py
+++ test_auto.py
@@ -1,27 +1,28 @@
 import subprocess
 import asyncio
+import sys
 
-async def run_spider(scrap, num):
+arg_1  = sys.argv[1]
+arg_2  = sys.argv[2]
+arg_3  = sys.argv[3]
+print(arg_1)
+print(arg_2)
+print(arg_3)
+
+async def run_spider(arg_1, arg_2, arg_3):
     while True:
         # start the Scrapy spider
-        process = subprocess.Popen(['scrapy', 'crawl', scrap, "-a", "u="+str(num)], cwd='C:\GIT\scrapy\demo\demo\spiders',  creationflags=subprocess.CREATE_NEW_CONSOLE)
+        
+        path  = "C:\\GIT\\scrapy\\"+arg_3+"\\"+arg_3+"\\spiders"
+        process = subprocess.Popen(['scrapy', 'crawl', arg_1, "-a", "u="+str(arg_2)], cwd=path,  creationflags=subprocess.CREATE_NEW_CONSOLE)
         process.wait()
         
         # wait for a few seconds before restarting the spider
-        print(f"{scrap} - {num} se va a reiniciar en 10 segundos ")
+        print(f"{arg_1} - {arg_2} se va a reiniciar en 10 segundos ")
         await asyncio.sleep(10)
         
-        
-async def main():
-    # create tasks for each spider and number combination
-    tasks = [
-        asyncio.create_task(run_spider('saga', 1)),
-        asyncio.create_task(run_spider('saga', 2)),
-    ]
 
-    # run the tasks concurrently
-    await asyncio.gather(*tasks)
     
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(run_spider(arg_1,arg_2, arg_3))
