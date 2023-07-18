2d3eaae90dcb7c2f17e1969443e5953571abe0c2
jechs83 <jechs83@gmail.com>
2023-07-17 21:41:42 -0500

    dad

--- ripley/ripley/spiders/ripple_scrap.py
+++ ripley/ripley/spiders/ripple_scrap.py
@@ -112,5 +112,6 @@ class RippleScrapSpider(scrapy.Spider):
                 item["date"]= load_datetime()[0]
                 item["time"]= load_datetime()[1]
                 item["home_list"] = "https://www.ripley.com.pe/"
+                time.sleep(0.5)
             
                 yield item
