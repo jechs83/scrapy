73f2bd8b103f00de6e0da4538c8723619cf55941
jechs83 <jechs83@gmail.com>
2023-10-04 03:57:54 -0500

    update unique ide price history saga

--- saga.bat
+++ saga.bat
@@ -17,10 +17,7 @@ start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 16 demo
 start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 17 demo
 start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 18 demo
 start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 19 demo
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 20 demo
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 21 demo
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 22 demo
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 23 demo
+
 
 
 start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py pro 1 promart
