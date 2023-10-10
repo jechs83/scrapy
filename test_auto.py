f2369afd29f5fef99831f8ceae347609852961b0
jechs83 <jechs83@gmail.com>
2023-10-10 10:24:29 -0500

    update

--- test_auto.py
+++ test_auto.py
@@ -1,20 +1,25 @@
 import subprocess
 import asyncio
 import sys
+import time
 
 arg_1  = sys.argv[1]
 arg_2  = sys.argv[2]
 arg_3  = sys.argv[3]
+arg_4  = sys.argv[4]
 print(arg_1)
 print(arg_2)
 print(arg_3)
+print(arg_4)
 
-async def run_spider(arg_1, arg_2, arg_3):
+
+
+async def run_spider(arg_1, arg_2, arg_3, arg_4):
     while True:
         # start the Scrapy spider
 
         path  = "C:\\GIT\\scrapy\\"+arg_3+"\\"+arg_3+"\\spiders"
-        process = subprocess.Popen(['scrapy', 'crawl', arg_1, "-a", "u="+str(arg_2)], cwd=path,  creationflags=subprocess.CREATE_NEW_CONSOLE)
+        process = subprocess.Popen(['scrapy', 'crawl', arg_1, "-a", "u="+str(arg_2), "-a", "u="+str(arg_4) ], cwd=path,  creationflags=subprocess.CREATE_NEW_CONSOLE)
         process.wait()
 
         # wait for a few seconds before restarting the spider
@@ -25,4 +30,4 @@ async def run_spider(arg_1, arg_2, arg_3):
 
 
 if __name__ == '__main__':
-    asyncio.run(run_spider(arg_1,arg_2, arg_3))
+    asyncio.run(run_spider(arg_1,arg_2, arg_3, arg_4))
