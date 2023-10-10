ebd2af60d394a904080e90fa7076147ea894e9a7
javier <jechs83@gmail.com>
2023-10-10 10:35:43 -0500

    update

--- saga4.bat
+++ saga4.bat
@@ -1,2 +1,28 @@
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 1  demo 0
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 1 demo 1
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 2 demo 
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 3 demo
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 4 demo
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 5 demo
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 6 demo
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 7 demo
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 8 demo
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 9 demo
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 10 demo
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 11 demo
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 12 demo
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 13 demo 0
+
+
+
+
+
+
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py pro 1 promart
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py tai 1 tailoy
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py pro 1 promart
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py vea 1 plazavea
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py metro1 1 metro
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py oh 1 oechsle
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py oh 2 oechsle
+
 
