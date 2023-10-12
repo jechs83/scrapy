c34a9149a2a97f5f475813dfe678b871a99f5759
jechs83 <jechs83@gmail.com>
2023-10-12 01:12:26 -0500

    udate

--- demo/demo/spiders/saga.py
+++ demo/demo/spiders/saga.py
@@ -5,6 +5,8 @@ from datetime import datetime
 from datetime import date
 from  demo.spiders import url_list 
 import uuid
+from decouple import config
+import pymongo
 
 
 
@@ -23,8 +25,59 @@ class SagaSpider(scrapy.Spider):
     allowed_domains = ["falabella.com.pe"]
 
 
+    def __init__(self, *args, **kwargs):
+        super(SagaSpider, self).__init__(*args, **kwargs)
+        self.client = pymongo.MongoClient(config("MONGODB"))
+        self.db = self.client["brand_allowed"]
+        self.lista = self.brand_allowed()[int(self.b)]  # Initialize self.lista based on self.b
+
+    def brand_allowed(self):
+        collection1 = self.db["shoes"]
+        collection2 = self.db["electro"]
+        collection3 = self.db["tv"]
+        collection4 = self.db["cellphone"]
+        collection5 = self.db["laptop"]
+        collection6 = self.db["consola"]
+        collection7 = self.db["audio"]
+        collection8 = self.db["colchon"]
+        collection9 = self.db["nada"]
+        collection10 = self.db["sport"]
+        collection11 = self.db["curacao"]
+        collection12 = self.db["todo"]
+        
+        shoes = collection1.find({})
+        electro = collection2.find({})
+        tv = collection3.find({})
+        cellphone = collection4.find({})
+        laptop = collection5.find({})
+        consola = collection6.find({})
+        audio = collection7.find({})
+        colchon = collection8.find({})
+        nada = collection9.find({})
+        sport = collection10.find({})
+        curacao = collection11.find({})
+        todo = collection12.find({})
+
+
+        shoes_list = [doc["brand"] for doc in shoes]
+        electro_list = [doc["brand"] for doc in electro]
+        tv_list = [doc["brand"] for doc in tv]
+        cellphone_list = [doc["brand"] for doc in cellphone]
+        laptop_list = [doc["brand"] for doc in laptop]
+        consola_list = [doc["brand"] for doc in consola]
+        audio_list = [doc["brand"] for doc in audio]
+        colchon_list = [doc["brand"] for doc in colchon]
+        nada_list = [doc["brand"] for doc in nada]
+        sport_list = [doc["brand"] for doc in sport]
+        curacao_list = [doc["brand"] for doc in curacao]
+        todo_list = [doc["brand"] for doc in todo]
+        return (shoes_list ,electro_list,tv_list,cellphone_list,laptop_list, consola_list, 
+                audio_list, colchon_list,nada_list,sport_list,curacao_list, todo_list)
+    
+
     def start_requests(self):
         u = int(getattr(self, 'u', '0'))
+        b = int(getattr(self, 'b', '0'))
 
         # Define a dictionary to map 'u' values to the corresponding url_list
         url_mapping = {
@@ -91,13 +144,19 @@ class SagaSpider(scrapy.Spider):
                 
 
                
+                # try:
                 try:
                     item["brand"]= i["brand"]
-                    product = item["brand"]
-                    if product.lower() in ["GENERICO", "generico", "GENERICA", "generica","GENÉRICO","GENÉRICA", "GENERIC" , "genérico","genérica"]:
+                except:   item["brand"] = None
+                product = item["brand"]
+                if self.lista == []:
+                    pass
+                else:
+                    if product.lower() not in self.lista:
                         continue
+            
                     
-                except: item["brand"]= None
+                # except: item["brand"]= None
 
                
                 
