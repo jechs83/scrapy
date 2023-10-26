5c38f11279ade08fab33c4db65a1a907853a4414
jechs83 <jechs83@gmail.com>
2023-10-26 00:13:15 -0500

    pasa

--- saga3.bat
+++ saga3.bat
@@ -2,24 +2,24 @@
 ::start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 200 demo
 ::start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 300 demostart "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 100 ripley
 
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 1 ripley
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 2 ripley
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 3 ripley
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 4 ripley
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 5 ripley
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 6 ripley
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 7 ripley
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 8 ripley
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 9 ripley
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 1 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 2 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 3 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 4 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 5 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 6 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 7 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 8 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 9 ripley 8
 
 
 
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 1 curacao
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 2 curacao
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 3 curacao
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 4 curacao
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 5 curacao
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 6 curacao
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 7 curacao
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 8 curacao
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 9 curacao
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 1 curacao 10
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 2 curacao 10
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 3 curacao 10
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 4 curacao 10
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 5 curacao 10
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 6 curacao 10
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 7 curacao 10
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 8 curacao 10
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 9 curacao 10
