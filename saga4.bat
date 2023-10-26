38ec42da89c5d065f10000f7a00686bc9ad4e932
jechs83 <jechs83@gmail.com>
2023-10-26 02:13:02 -0500

    pdate

--- saga4.bat
+++ saga4.bat
@@ -25,6 +25,17 @@ start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 6 curacao 8
 start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 7 curacao 8
 start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py cura 8 curacao 10
 
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 1 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 2 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 3 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 4 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 5 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 6 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 7 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 8 ripley 8
+start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py ripley_scrap 9 ripley 8
+
+
 
 REM shoes_list 0 ,electro_list 1 ,tv_list 2 ,cellphone_list 3, laptop_list 4, consola-list 5, audio_list 6, colchon_list 7,  
 REM nada_list 8,  
