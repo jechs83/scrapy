c99da88c9e29aa58f637915c68b62c70e0c2f252
jechs83 <jechs83@gmail.com>
2023-10-27 11:12:27 -0500

    upde

--- saga.bat
+++ saga.bat
@@ -15,23 +15,14 @@ start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 14 demo 0
 start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 15 demo 0
 start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 16 demo 0
 start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 17 demo 0
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 18 demo 0
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 19 demo 0
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 20 demo 0
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 21 demo 0
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 22 demo 0
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py saga 23 demo 0
-
-
-
 
 
 start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py pro 1 promart 0
 start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py tai 1 tailoy 0
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py pro 1 promart 0
+
 start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py vea 1 plazavea 0
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py metro1 1 metro 0
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py oh 1 oechsle 0
-start "" /D "C:\GIT\scrapy" cmd /k python test_auto.py oh 2 oechsle 0
+
+
+
 
 
