dd29266e35f78c92d3892e06ae7457b5b84b45ab
jechs83 <jechs83@gmail.com>
2023-11-13 15:38:23 -0500

    dad

--- bat_files/tailoy1.bat
+++ bat_files/tailoy1.bat
@@ -7,7 +7,7 @@ rem Loop to run the spider multiple times
 
 
 
-cd C:\Git\scrapy\tailoy\promatailoyrt\spiders\
+cd C:\Git\scrapy\tailoy\tailoy\spiders\
 
 for /l %%i in (1,1,%num_runs%) do (
     scrapy crawl tai -a u=1 -a b=0
