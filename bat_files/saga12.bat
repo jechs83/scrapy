dbcdf146be555ba2776db857a96959bf3c55b05e
unknown <jechs83@gmail.com>
2023-11-13 15:32:19 -0500

    sas

---
+++ bat_files/saga12.bat
@@ -0,0 +1,14 @@
+@echo off
+
+rem Set the number of times to run the spider
+set num_runs=10000
+
+rem Loop to run the spider multiple times
+
+
+
+cd C:\Git\scrapy\demo\demo\spiders\
+
+for /l %%i in (1,1,%num_runs%) do (
+    scrapy crawl saga -a u=12 -a b=0
+)
