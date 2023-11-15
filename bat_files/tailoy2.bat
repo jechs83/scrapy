bb9b4954c7210040c0bb9642243df687bcefdfc2
jechs83 <jechs83@gmail.com>
2023-11-15 08:15:09 -0500

    asas

--- bat_files/tailoy2.bat
+++ bat_files/tailoy2.bat
@@ -1,14 +1,11 @@
-@echo off
-
-rem Set the number of times to run the spider
-set num_runs=100000000000000000000000000
-
-rem Loop to run the spider multiple times
 
 
+rem Loop to run the spider indefinitely
 
 cd C:\Git\scrapy\tailoy\tailoy\spiders\
 
-for /l %%i in (1,1,%num_runs%) do (
-    scrapy crawl tai -a u=2 -a b=0
-)
+:loop
+scrapy crawl tai -a u=2 -a b=0
+goto loop
+
+
