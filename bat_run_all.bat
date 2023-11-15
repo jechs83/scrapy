a39e84e351b785b665126d1139f057c733944a53
jechs83 <jechs83@gmail.com>
2023-11-15 17:52:02 -0500

    sasa

--- bat_run_all.bat
+++ bat_run_all.bat
@@ -22,6 +22,26 @@ start cmd /k call C:\Git\scrapy\bat_files\saga19.bat
 
 start cmd /k call C:\Git\scrapy\bat_files\promart1.bat
 start cmd /k call C:\Git\scrapy\bat_files\tailoy1.bat
+start cmd /k call C:\Git\scrapy\bat_files\tailoy2.bat
+start cmd /k call C:\Git\scrapy\bat_files\tailoy3.bat
+
 start cmd /k call C:\Git\scrapy\bat_files\plazavea1.bat
+
+start cmd /k call C:\Git\scrapy\bat_files\juntoz1.bat
+start cmd /k call C:\Git\scrapy\bat_files\platanitos1.bat
+
+start cmd /k call C:\Git\scrapy\bat_files\oh1.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh2.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh3.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh4.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh5.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh6.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh7.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh8.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh9.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh10.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh11.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh12.bat
+
 
 
--- bat_run_all3.bat
+++ bat_run_all3.bat
@@ -1,9 +1,9 @@
 @echo off
 
-start cmd /k call C:\Git\scrapy\bat_files\tailoy1.bat
-start cmd /k call C:\Git\scrapy\bat_files\tailoy2.bat
-start cmd /k call C:\Git\scrapy\bat_files\tailoy3.bat
+rem start cmd /k call C:\Git\scrapy\bat_files\tailoy1.bat
+rem start cmd /k call C:\Git\scrapy\bat_files\tailoy2.bat
+rem start cmd /k call C:\Git\scrapy\bat_files\tailoy3.bat
 
-start cmd /k call C:\Git\scrapy\bat_files\juntoz1.bat
+rem start cmd /k call C:\Git\scrapy\bat_files\juntoz1.bat
 
-start cmd /k call C:\Git\scrapy\bat_files\platanitos1.bat
+rem start cmd /k call C:\Git\scrapy\bat_files\platanitos1.bat
