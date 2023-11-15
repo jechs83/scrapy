80733584d03f2b6f4fde59bdbe114771b73a47df
jechs83 <jechs83@gmail.com>
2023-11-15 03:20:00 -0500

    usas

--- bat_run_all3.bat
+++ bat_run_all3.bat
@@ -2,6 +2,6 @@
 
 start cmd /k call C:\Git\scrapy\bat_files\tailoy1.bat
 start cmd /k call C:\Git\scrapy\bat_files\tailoy2.bat
-start cmd /k call C:\Git\scrapy\bat_files\tailoy3bat
+start cmd /k call C:\Git\scrapy\bat_files\tailoy3.bat
 
 start cmd /k call C:\Git\scrapy\bat_files\juntoz1.bat
