017690a41d018ace3f02c461bf301fc4b5e7cc0b
jechs83 <jechs83@gmail.com>
2023-11-16 09:35:58 -0500

    sas

--- test_autopppp.py
+++ test_autopppp.py
@@ -17,8 +17,8 @@ async def run_spider(arg_1, arg_2, arg_3, arg_4):
         try:
             # start the Scrapy spider
             path  = "C:\\GIT\\scrapy\\"+arg_3+"\\"+arg_3+"\\spiders"
-            process = subprocess.Popen(['scrapy', 'crawl', arg_1, "-a", "u="+str(arg_2), "-a", "b="+str(arg_4) ], cwd=path,  creationflags=subprocess.CREATE_NEW_CONSOLE)
-            process.wait()
+            process = await asyncio.subprocess.Popen(['scrapy', 'crawl', arg_1, "-a", "u="+str(arg_2), "-a", "b="+str(arg_4) ], cwd=path,  creationflags=subprocess.CREATE_NEW_CONSOLE)
+            await process.wait()
             # wait for a few seconds before restarting the spider
             print(f"{arg_1} - {arg_2} se va a reiniciar en 10 segundos ")
             await asyncio.sleep(10)
