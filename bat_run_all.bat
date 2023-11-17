0b1f76a138ccd24df70c3abc31c07da931a0e749
jechs83 <jechs83@gmail.com>
2023-11-16 22:18:48 -0500

    sas

--- bat_run_all.bat
+++ bat_run_all.bat
@@ -2,41 +2,41 @@
 
 start cmd /k call C:\Git\scrapy\bat_files\saga1.bat
 start cmd /k call C:\Git\scrapy\bat_files\saga2.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga3.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga4.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga5.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga6.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga7.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga8.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga9.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga10.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga11.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga12.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga13.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga14.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga15.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga16.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga17.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga18.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\saga19.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\promart1.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\tailoy1.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\tailoy2.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\tailoy3.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\plazavea1.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\juntoz1.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\platanitos1.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\oh1.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\oh2.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\oh3.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\oh4.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\oh5.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\oh6.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\oh7.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\oh8.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\oh9.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\oh10.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\oh11.bat
-rem start cmd /k call C:\Git\scrapy\bat_files\oh12.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga3.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga4.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga5.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga6.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga7.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga8.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga9.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga10.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga11.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga12.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga13.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga14.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga15.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga16.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga17.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga18.bat
+start cmd /k call C:\Git\scrapy\bat_files\saga19.bat
+start cmd /k call C:\Git\scrapy\bat_files\promart1.bat
+start cmd /k call C:\Git\scrapy\bat_files\tailoy1.bat
+start cmd /k call C:\Git\scrapy\bat_files\tailoy2.bat
+start cmd /k call C:\Git\scrapy\bat_files\tailoy3.bat
+start cmd /k call C:\Git\scrapy\bat_files\plazavea1.bat
+start cmd /k call C:\Git\scrapy\bat_files\juntoz1.bat
+start cmd /k call C:\Git\scrapy\bat_files\platanitos1.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh1.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh2.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh3.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh4.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh5.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh6.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh7.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh8.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh9.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh10.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh11.bat
+start cmd /k call C:\Git\scrapy\bat_files\oh12.bat
 
 
