b23068fc00caf63f99e1420fbdcdb0940b458576
jechs83 <jechs83@gmail.com>
2023-11-19 11:48:14 -0500

    sa

--- bat_run_all2.bat
+++ bat_run_all2.bat
@@ -30,7 +30,6 @@ start cmd /k call C:\Git\scrapy\bat_files\promart1.bat
 start cmd /k call C:\Git\scrapy\bat_files\tailoy1.bat
 start cmd /k call C:\Git\scrapy\bat_files\tailoy2.bat
 start cmd /k call C:\Git\scrapy\bat_files\tailoy3.bat
-start cmd /k call C:\Git\scrapy\bat_files\plazavea1.bat
 start cmd /k call C:\Git\scrapy\bat_files\juntoz1.bat
 start cmd /k call C:\Git\scrapy\bat_files\platanitos1.bat
 
