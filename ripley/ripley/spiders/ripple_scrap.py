729c4560b2a7de9466b6266845f406121775deed
jechs83 <jechs83@gmail.com>
2023-11-19 21:36:00 -0500

    as

--- ripley/ripley/spiders/ripple_scrap.py
+++ ripley/ripley/spiders/ripple_scrap.py
@@ -40,44 +40,13 @@ class RippleScrapSpider(scrapy.Spider):
         super(RippleScrapSpider, self).__init__(*args, **kwargs)
         self.client = pymongo.MongoClient(config("MONGODB"))
         self.db = self.client["brand_allowed"]
-        self.lista = self.brand_allowed()[int(self.b)]  # Initialize self.lista based on self.b
+        self.lista = self.brand_allowed()# Initialize self.lista based on self.b
 
     def brand_allowed(self):
         collection1 = self.db["todo"]
-        collection2 = self.db["electro"]
-        collection3 = self.db["tv"]
-        collection4 = self.db["cellphone"]
-        collection5 = self.db["laptop"]
-        collection6 = self.db["consola"]
-        collection7 = self.db["audio"]
-        collection8 = self.db["colchon"]
-        collection9 = self.db["nada"]
-        collection10 = self.db["sport"]
-        
         shoes = collection1.find({})
-        electro = collection2.find({})
-        tv = collection3.find({})
-        cellphone = collection4.find({})
-        laptop = collection5.find({})
-        consola = collection6.find({})
-        audio = collection7.find({})
-        colchon = collection8.find({})
-        nada = collection9.find({})
-        sport = collection10.find({})
-
-
         shoes_list = [doc["brand"] for doc in shoes]
-        electro_list = [doc["brand"] for doc in electro]
-        tv_list = [doc["brand"] for doc in tv]
-        cellphone_list = [doc["brand"] for doc in cellphone]
-        laptop_list = [doc["brand"] for doc in laptop]
-        consola_list = [doc["brand"] for doc in consola]
-        audio_list = [doc["brand"] for doc in audio]
-        colchon_list = [doc["brand"] for doc in colchon]
-        nada_list = [doc["brand"] for doc in nada]
-        sport_list = [doc["brand"] for doc in sport]
-        return shoes_list ,electro_list,tv_list,cellphone_list,laptop_list, consola_list, audio_list, colchon_list,nada_list,sport_list
-    
+        return shoes_list 
     
     links =[]
     def start_requests(self):
@@ -105,13 +74,9 @@ class RippleScrapSpider(scrapy.Spider):
                 user_agent = random.choice(self.user_agents)
 
                 headers = {'User-Agent': user_agent}
-
+                
                 yield scrapy.Request(url, self.parse, headers=headers   )
-
-      
-         
         
-
             
     def parse(self, response):
             item = RipleyItem()
@@ -128,8 +93,6 @@ class RippleScrapSpider(scrapy.Spider):
                 else:
                     if product.lower() not in self.lista:
                         continue
-
-                
                 try:
                     item["product"] = i.css(".catalog-product-details__name::text").get()
                 except: item["product"] = None
@@ -148,7 +111,6 @@ class RippleScrapSpider(scrapy.Spider):
 
                 item["_id"] =  item["sku"]
 
-                
                 try:
                     item["web_dsct"] = round(float(i.css(".catalog-product-details__discount-tag::text").get().replace("-", "").replace("%", "")))
                 except:
@@ -180,12 +142,7 @@ class RippleScrapSpider(scrapy.Spider):
                 item["date"]= load_datetime()[0]
                 item["time"]= load_datetime()[1]
                 item["home_list"] = response.url       
-            
-
-               
-                
+        
                 yield item
-           
-
-
+         
        
