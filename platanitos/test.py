3369ce7eb2ded89c04a8a06c20167d84e04b3874
jechs83 <jechs83@gmail.com>
2023-11-21 10:44:55 -0500

    sas

---
+++ platanitos/test.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# Replace 'other_script.py' with the name of your Python file
+file_to_execute = '/Users/javier/GIT/scrapy_saga/curacao/run.py'
+
+# Run the Python file using subprocess
+subprocess.run(['python', file_to_execute])
