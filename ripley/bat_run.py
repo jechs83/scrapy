76829e669d60d865ee7d2beba456ef248d4fae97
jechs83 <jechs83@gmail.com>
2024-01-09 02:13:30 -0500

    refactoriando ripey

--- ripley/bat_run.py
+++ ripley/bat_run.py
@@ -2,33 +2,33 @@
 
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-from demo.spiders.saga import SagaSpider
+from ripley.spiders.ripple_scrap import RippleScrapSpider
 import subprocess
 
 
 def run_spider_with_parameters( b_value):
     process = CrawlerProcess(get_project_settings())
 
-    process.crawl(SagaSpider, u=1, b=b_value)
-    process.crawl(SagaSpider, u=2, b=b_value)
-    process.crawl(SagaSpider, u=3, b=b_value)
-    process.crawl(SagaSpider, u=4, b=b_value)
-    process.crawl(SagaSpider, u=5, b=b_value)
-    process.crawl(SagaSpider, u=6, b=b_value)
-    process.crawl(SagaSpider, u=7, b=b_value)
-    process.crawl(SagaSpider, u=8, b=b_value)
-    process.crawl(SagaSpider, u=9, b=b_value)
-    process.crawl(SagaSpider, u=10, b=b_value)
-    process.crawl(SagaSpider, u=11, b=b_value)
-    process.crawl(SagaSpider, u=12, b=b_value)
-    process.crawl(SagaSpider, u=13, b=b_value)
-    process.crawl(SagaSpider, u=14, b=b_value)
-    process.crawl(SagaSpider, u=15, b=b_value)
-    process.crawl(SagaSpider, u=16, b=b_value)
-    process.crawl(SagaSpider, u=17, b=b_value)
-    process.crawl(SagaSpider, u=18, b=b_value)
-    process.crawl(SagaSpider, u=19, b=b_value)
-    process.crawl(SagaSpider, u=20, b=b_value)
+    process.crawl(RippleScrapSpider, u=1, b=b_value)
+    process.crawl(RippleScrapSpider, u=2, b=b_value)
+    process.crawl(RippleScrapSpider, u=3, b=b_value)
+    process.crawl(RippleScrapSpider, u=4, b=b_value)
+    process.crawl(RippleScrapSpider, u=5, b=b_value)
+    process.crawl(RippleScrapSpider, u=6, b=b_value)
+    process.crawl(RippleScrapSpider, u=7, b=b_value)
+    process.crawl(RippleScrapSpider, u=8, b=b_value)
+    process.crawl(RippleScrapSpider, u=9, b=b_value)
+    process.crawl(RippleScrapSpider, u=10, b=b_value)
+    process.crawl(RippleScrapSpider, u=11, b=b_value)
+    process.crawl(RippleScrapSpider, u=12, b=b_value)
+    process.crawl(RippleScrapSpider, u=13, b=b_value)
+    process.crawl(RippleScrapSpider, u=14, b=b_value)
+    process.crawl(RippleScrapSpider, u=15, b=b_value)
+    process.crawl(RippleScrapSpider, u=16, b=b_value)
+    process.crawl(RippleScrapSpider, u=17, b=b_value)
+    process.crawl(RippleScrapSpider, u=18, b=b_value)
+    process.crawl(RippleScrapSpider, u=19, b=b_value)
+    process.crawl(RippleScrapSpider, u=20, b=b_value)
 
     process.start()
 
@@ -36,6 +36,6 @@ if __name__ == "__main__":
     b_value = 0
 
     run_spider_with_parameters(b_value)
-    subprocess.run("pkill -f 'python saga_s.py'")
+    subprocess.run("pkill -f 'python bat_run.py'")
 
 
