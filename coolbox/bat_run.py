684cd4edae71d3aa8f367d7488944f3ff4cb6fc5
jechs83 <jechs83@gmail.com>
2024-01-09 03:07:50 -0500

    refactor coolbox

--- coolbox/bat_run.py
+++ coolbox/bat_run.py
@@ -3,7 +3,6 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from coolbox.spiders.cool import CoolboxSpider
-
 import subprocess
 
 
@@ -11,16 +10,13 @@ def run_spider_with_parameters( b_value):
     process = CrawlerProcess(get_project_settings())
 
     process.crawl(CoolboxSpider, u=1, b=b_value)
-    process.crawl(CoolboxSpider, u=2, b=b_value)
- 
-   
- 
+
     process.start()
 
 if __name__ == "__main__":
-    b_value = 6
+    b_value = 0
 
     run_spider_with_parameters(b_value)
-    subprocess.run("pkill -f 'python test_s.py'")
+    subprocess.run("pkill -f 'python main_bat_run.py'")
 
 
