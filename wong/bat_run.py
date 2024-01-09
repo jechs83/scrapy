5ffbd7afb74e116f86d23727ccc4d8c67243dc63
jechs83 <jechs83@gmail.com>
2024-01-09 03:26:42 -0500

    refactoring WONG

--- wong/bat_run.py
+++ wong/bat_run.py
@@ -3,7 +3,6 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from wong.spiders.wong import WongSpider
-
 import subprocess
 
 
@@ -13,13 +12,14 @@ def run_spider_with_parameters( b_value):
     process.crawl(WongSpider, u=1, b=b_value)
     process.crawl(WongSpider, u=2, b=b_value)
   
-   
+
+  
     process.start()
 
 if __name__ == "__main__":
-    b_value = 4
+    b_value = 0
 
     run_spider_with_parameters(b_value)
-    subprocess.run("pkill -f 'python shop_s.py'")
+    subprocess.run("pkill -f 'python main_bat_run.py'")
 
 
