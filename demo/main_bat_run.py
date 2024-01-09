f7e7f6c6e97c2b908e1aac90753b6fda19857bd7
jechs83 <jechs83@gmail.com>
2024-01-09 01:52:46 -0500

    refactoriando comenzsado pro saga

--- demo/main_bat_run.py
+++ demo/main_bat_run.py
@@ -2,15 +2,15 @@ import subprocess
 import time
 
 def run_saga_script():
-    saga_script_path = "c:/git/scrapy/demo/saga_s.py"  # Absolute path to saga_s.py
+    saga_script_path = "c:/git/scrapy/demo/bat_run.py"  # Absolute path to saga_s.py
 
     while True:
-        print("Starting saga_s.py...")
+        print("COMENZO SAGA FALABELLA...")
         process = subprocess.Popen(["python", saga_script_path])  # Start saga_s.py as a subprocess
 
         while True:
             if process.poll() is not None:  # Check if the subprocess has completed
-                print("saga_s.py has stopped.")
+                print("PARO SAGA FALABELLA REINICIANDO...")
                 break  # Break the loop to restart the script
             time.sleep(5)  # Wait for 5 seconds before checking again
 
