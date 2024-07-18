b66e26f9149acf9611522114d5034d8db314928a
javier chung <jchung@sparxworks.com>
2024-07-18 11:42:08 -0500

    sas

--- demo/demo/spiders/saga.py
+++ demo/demo/spiders/saga.py
@@ -2,25 +2,33 @@ import scrapy
 import sys
 import json
 from demo.items import DemoItem
-from datetime import datetime, date
+from datetime import datetime
+from datetime import date
 import pymongo
-from demo.spiders.urls_db import links
+from demo.spiders.urls_db import *
 from decouple import config
 import time
 
+
+
+
 def load_datetime():
+    
     today = date.today()
     now = datetime.now()
     date_now = today.strftime("%d/%m/%Y")  
     time_now = now.strftime("%H:%M:%S")
+        
     return date_now, time_now, today
 
 current_day = load_datetime()[0]
 
 class SagaSpider(scrapy.Spider):
+    #list_to_skip = skip_brand()
     name = "saga"
     allowed_domains = ["falabella.com.pe"]
     handle_httpstatus_list = [200, 206]
+
 
     def __init__(self, *args, **kwargs):
         u = int(getattr(self, 'u', '0'))
@@ -28,8 +36,10 @@ class SagaSpider(scrapy.Spider):
         super(SagaSpider, self).__init__(*args, **kwargs)
         self.client = pymongo.MongoClient(config("MONGODB"))
         self.db = self.client["brand_allowed"]
-        self.lista = self.brand_allowed()  # Initialize self.lista based on self.b
+        self.lista = self.brand_allowed() # Initialize self.lista based on self.b
         self.urls = links()[int(int(self.u)-1)]
+   
+
     
     def brand_allowed(self):
         collection1 = self.db["todo"]
@@ -37,114 +47,130 @@ class SagaSpider(scrapy.Spider):
         shoes_list = [doc["brand"] for doc in shoes]
         collection1 = self.db["nada"]
         nada = collection1.find({})
-        return shoes_list, nada
+        return shoes_list ,nada
+
 
     def start_requests(self):
+       
         for i, v in enumerate(self.urls):
-            url = v[0]
-            yield scrapy.Request(url, self.parse, meta={'base_url': url, 'subdomain': self.get_subdomain(url), 'page': 1})
-
-    def get_subdomain(self, url):
-        if "tottus" in url:
-            return "tottus"
-        elif "sodimac" in url:
-            return "sodimac"
-        return ""
+            if "tottus" in v[0]:
+                for e in range (v[1]+10):
+                    url = v[0]+ "?subdomain=tottus&page="+str(e+1) +"&store=tottus"
+                    yield scrapy.Request(url, self.parse)
+            if "sodimac" in v[0]:
+                for e in range (v[1]+10):
+                    url = v[0]+ "?subdomain=sodimac&page="+str(e+1)+"&store=sodimac"
+                    yield scrapy.Request(url, self.parse)
+            else:
+                for e in range (v[1]+10):
+                    url = v[0]+ "?page="+str(e+1) 
+                    yield scrapy.Request(url, self.parse)
+                
 
     def parse(self, response):
+      
         if response.status != 200:
-            self.logger.warning(f"Skipping URL {response.url} due to non-200 status code: {response.status}")
-            return
-
+        # If the response status is not 200, skip processing this link and move to the next one
+                self.logger.warning(f"Skipping URL {response.url} due to non-200 status code: {response.status}")
+                return
+        
         if "/noResult" in response.url:
-            self.logger.info("Skipping this URL and moving to the next one.")
-            return
-
-        base_url = response.meta['base_url']
-        subdomain = response.meta['subdomain']
-        current_page = response.meta['page']
-
+                # Move to the next URL in the array (since it is a "noResult" page)
+                self.logger.info("Skipping this URL and moving to the next one.")
+                return
+    
         item = DemoItem()
+
+        # Find the script tag with the JSON data
         script_tag = response.xpath('//script[@id="__NEXT_DATA__"]/text()').get()
 
         if script_tag:
             json_content = json.loads(script_tag)
+
+            # Assuming the relevant JSON data is under "props" -> "pageProps" in the JSON response
             page_props = json_content.get('props', {}).get('pageProps', {}).get("results",{})
-        else:
-            page_props = {}
 
         productos = page_props
         
         for i in productos:
-            item["brand"] = i["brand"]
-            producto = item["brand"].lower()
+            
+               
+    
+                item["brand"]= i["brand"]
 
-            if self.lista[0] == []:
-                pass
-            else:
-                if producto not in self.lista[0]:
-                    continue
 
-            item["product"] = i["displayName"]
-            item["sku"] = i["skuId"]
-            item["_id"] = i["skuId"]
+        
+                producto = item["brand"].lower()
 
-            prices = i["prices"]
-            if len(prices) == 1:
-                item["best_price"] = 0
-                item["card_price"] = 0
-                item["list_price"] = float(prices[0]["price"][0].replace(",", ""))
-            elif len(prices) == 2:
-                item["best_price"] = float(prices[0]["price"][0].replace(",", ""))
-                item["card_price"] = 0
-                item["list_price"] = float(prices[1]["price"][0].replace(",", ""))
-            elif len(prices) == 3:
-                item["best_price"] = float(prices[1]["price"][0].replace(",", ""))
-                item["card_price"] = float(prices[0]["price"][0].replace(",", ""))
-                item["list_price"] = float(prices[2]["price"][0].replace(",", ""))
+                if self.lista[0] == []:
+                    pass
+                else:
+                    if producto not in self.lista[0]:
 
-            item["link"] = i["url"]
-            item["image"] = i.get("mediaUrls", [None])[0]
-            item["web_dsct"] = float(i.get("discountBadge", {}).get("label", "0").replace("-", "").replace("%", ""))
-            item["dsct_app"] = 1 if i.get("multipurposeBadges", [{}])[0].get("label") == "Dscto extra por app" else 0
+                        continue
 
-            if not (item["list_price"] or item["card_price"] or item["best_price"]):
-                continue
+                item["product"]=  i["displayName"]
 
-            item["market"] = "saga"
-            item["date"] = load_datetime()[0]
-            item["time"] = load_datetime()[1]
-            item["home_list"] = response.url
-            item["card_dsct"] = 0
+                item["sku"] = i["skuId"]
+                item["_id"] =i["skuId"]
 
-            yield item
+                if len(i["prices"])== 1:
 
-        # Detectar el número máximo de páginas si no está ya en los meta datos
-        if 'max_pages' not in response.meta:
-            max_pages = self.get_max_pages(response)
-            if max_pages:
-                response.meta['max_pages'] = max_pages
-            else:
-                return
+                    item["best_price"] = 0
+                    item["card_price"] = 0
+                    item["list_price"] = float(i["prices"][0]["price"][0].replace(",",""))
 
-        # Obtener el número máximo de páginas desde los meta datos
-        max_pages = response.meta['max_pages']
+                elif len(i["prices"])== 2:
+                    item["best_price"] = float(i["prices"][0]["price"][0].replace(",",""))
+                    item["card_price"] = 0
+                    item["list_price"] = float(i["prices"][1]["price"][0].replace(",",""))
 
-        # Continuar a la siguiente página si no se ha alcanzado el número máximo de páginas
-        if current_page < max_pages:
-            next_page = current_page + 1
-            next_page_url = f"{base_url}?subdomain={subdomain}&page={next_page}&store={subdomain}" if subdomain else f"{base_url}?page={next_page}"
-            yield scrapy.Request(next_page_url, self.parse, meta={'base_url': base_url, 'subdomain': subdomain, 'page': next_page, 'max_pages': max_pages})
+                elif len(i["prices"])== 3:
+                    item["best_price"] = float(i["prices"][1]["price"][0].replace(",",""))
+                    item["card_price"] = float(i["prices"][0]["price"][0].replace(",",""))
+                    item["list_price"] = float(i["prices"][2]["price"][0].replace(",",""))
 
-    def get_max_pages(self, response):
-        pagination = response.xpath('//ul[@class="pagination"]/li/a/text()').extract()
-        if pagination:
-            try:
-                max_page = max(int(num) for num in pagination if num.isdigit())
-                return max_page
-            except ValueError:
-                return None
-        return None
+             
 
-# Para ejecutar el spider, guarda este código en un archivo llamado `saga_spider.py` y usa el comando:
-# scrapy runspider saga_spider.py
+                item["link"]=i["url"]
+
+                try:
+                 item["image"]=i["mediaUrls"][0]
+                except:
+                 item["image"]=str(i["mediaUrls"])
+        
+                try:
+                 item["web_dsct"]=float(i["discountBadge"]["label"].replace("-","").replace("%",""))
+                except:
+                        item["web_dsct"]=0
+
+
+                try:
+                    item["dsct_app"] = i["multipurposeBadges"][0]["label"]
+                
+                    if item["dsct_app"]=="Dscto extra por app" :
+                        item["dsct_app"]  = 1
+                    else:
+                        item["dsct_app"] = 0
+                except: 
+                    item["dsct_app"] = 0
+
+                if item["list_price"] and item["card_price"] and item["best_price"] == 0:
+                 continue
+
+                # if item["dsct_app"] and item["web_dsct"] >0 <=59:
+                #     continue
+
+                item["market"]= "saga"
+                item["date"]= load_datetime()[0]
+                item["time"]= load_datetime()[1]
+                item["home_list"] = response.url
+                item["card_dsct"] = 0
+
+                yield item
+         
+
+            
+
+
+
